e5fbc4070417f64aae891fd7c1d8acd32efc5968
yueze <2860339200@qq.com>
2022-10-13 18:29:52 +0800

    点击课程图片跳转测试和测试push

--- app/src/main/java/com/example/myapp/CourseListAdapter.java
+++ app/src/main/java/com/example/myapp/CourseListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.myapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -63,6 +65,18 @@ public class CourseListAdapter
 //        Bitmap bitmap=getHttpBitmap(hhh);
 //        holder.image.setImageBitmap(bitmap);
 //        holder.image.setB
+        holder.image.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Toast.makeText(view.getContext(),"哈哈哈哈哈哈",Toast.LENGTH_LONG).show();
+                        Intent intent=new Intent(view.getContext(),Login.class);
+                        view.getContext().startActivity(intent);
+
+                    }
+                }
+        );
         Log.d(TAG, "onBindViewHolder: "+recordsBean.getCollegeName());
 
     }
